5b18608643c8ed8be8fcb1dec5ebb268a529ab9a
Andre Lucas <andrelucca99@gmail.com>
2023-03-12 09:04:36 -0300

    add component blog

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import Clients from './Clients';
-import Projects from './Projects';
-import Specialist from './Specialist';
+import React from "react";
+import Blog from "./Blog";
+import Clients from "./Clients";
+import Projects from "./Projects";
+import Specialist from "./Specialist";
 import "./styles/main.css";
 
 function Main() {
   return (
     <main>
-        <section className="title-services">
-            <p>AGÊNCIA DE MARKETING DIGITAL</p>
-            <h2>CONHEÇA OS SERVIÇOS OFERECIDOS PELO FORMIGUEIRO</h2>
-        </section>
-        <section className="image-services">
-            <img src="https://formiga.digital/wp-content/uploads/2022/11/formigueiro-sem-fundo-1-1536x542.png" alt="foto" />
-        </section>
-        <section className="section-client">
-          <Clients />
-        </section>
-        <section className="section-projects">
-          <Projects />
-        </section>
+      <section className="title-services">
+        <p>AGÊNCIA DE MARKETING DIGITAL</p>
+        <h2>CONHEÇA OS SERVIÇOS OFERECIDOS PELO FORMIGUEIRO</h2>
+      </section>
+      <section className="image-services">
+        <img
+          src="https://formiga.digital/wp-content/uploads/2022/11/formigueiro-sem-fundo-1-1536x542.png"
+          alt="foto"
+        />
+      </section>
+      <section className="section-client">
+        <Clients />
+      </section>
+      <section className="section-projects">
+        <Projects />
+      </section>
 
-        <section className="section-specialist">
-          <Specialist />
-        </section>
+      <section className="section-specialist">
+        <Specialist />
+      </section>
+
+      <section className="section-blog">
+        <Blog />
+      </section>
     </main>
   );
 }
