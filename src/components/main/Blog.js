e4636719966f19f0f1d40bb4d432bfb6d1f3962d
Andre Lucas <andrelucca99@gmail.com>
2023-03-17 12:25:18 -0300

    refactor: alteração do componente Blog e add mock

--- src/components/main/Blog.js
+++ src/components/main/Blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from '../utils/Image';
 import TitlteLeft from '../utils/TitlteLeft';
 import './styles/blog.css';
+import { blog } from '../utils/mocks/Blog';
 
 function Blog() {
   return (
@@ -12,82 +13,15 @@ function Blog() {
       </TitlteLeft>
 
       <section className="images-blog">
-        <figure className="image-blog">
-          <Image image="https://formiga.digital/wp-content/uploads/2023/02/Dicas-para-melhorar-a-presenca_-1.jpg" alt="foto" />
-          <figcaption>
-            <h3>Dicas para melhorar a presença digital de uma empresa</h3>
-            <p>
-              Aqui estão algumas dicas: Definir objetivos claros: Antes de
-              começar a investir em marketing digital, é importante definir
-              objetivos claros e mensuráveis para sua presença
-            </p>
-          </figcaption>
-        </figure>
-
-        <figure className="image-blog">
-          <Image image="https://formiga.digital/wp-content/uploads/2023/02/como-medir-o-sucesso_-1.jpg" alt="foto" />
-          <figcaption>
-            <h3>Como medir o sucesso de uma campanha de marketing digital?</h3>
-            <p>
-              Medir o sucesso de uma campanha de marketing digital é importante
-              para avaliar o retorno do investimento e ajustar a estratégia de
-              marketing, se necessário.
-            </p>
-          </figcaption>
-        </figure>
-
-        <figure className="image-blog">
-          <Image image="https://formiga.digital/wp-content/uploads/2023/02/O-impacto-das-midias-sociais_.jpg" alt="foto" />
-          <figcaption>
-            <h3>
-              O impacto das mídias sociais na estratégia de marketing de uma
-              empresa.
-            </h3>
-            <p>
-              As mídias sociais têm um impacto significativo na estratégia de
-              marketing de uma empresa. Aqui estão algumas maneiras pelas quais
-              as mídias sociais podem afetar
-            </p>
-          </figcaption>
-        </figure>
-      </section>
-
-      <section className="images-blog">
-        <figure className="image-blog">
-          <Image image="https://formiga.digital/wp-content/uploads/2023/02/Tendencias-do-Mkt.jpg" alt="foto" />
-          <figcaption>
-            <h3>Tendências do Marketing Digital</h3>
-            <p>
-              Existem várias tendências atuais no marketing digital, mas aqui
-              estão algumas das principais: Conteúdo personalizado: Com o grande
-              volume de informações disponíveis na internet, as
-            </p>
-          </figcaption>
-        </figure>
-
-        <figure className="image-blog">
-          <Image image="https://formiga.digital/wp-content/uploads/2023/02/5-Social-Selling.jpg" alt="foto" />
-          <figcaption>
-            <h3>Social Selling: a estratégia de vendas nas redes sociais</h3>
-            <p>
-              O Social Selling é uma estratégia de vendas que utiliza as redes
-              sociais para se relacionar com potenciais clientes, gerar leads e
-              fechar negócios. É
-            </p>
-          </figcaption>
-        </figure>
-
-        <figure className="image-blog">
-          <Image image="https://formiga.digital/wp-content/uploads/2023/02/4-formigas.jpg" alt="foto" />
-          <figcaption>
-            <h3>O que podemos aprender com as formigas?</h3>
-            <p>
-              As formigas são insetos fascinantes que vivem em colônias
-              altamente organizadas e trabalham juntas para sobreviver e
-              prosperar. Há muitas lições que podemos aprender com
-            </p>
-          </figcaption>
-        </figure>
+        {blog.map((item) => (
+          <figure className="image-blog" key={ item }>
+            <Image image={ item.image } alt={ item.alt } />
+            <figcaption>
+              <h3>{ item.title }</h3>
+              <p>{ item.paragrafh }</p>
+            </figcaption>
+          </figure>
+        ))}
       </section>
     </section>
   );
---
+++ src/components/utils/mocks/Blog.js
@@ -0,0 +1,57 @@
+/* eslint-disable max-len */
+export const blog = [
+  {
+    id: 1,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/02/Dicas-para-melhorar-a-presenca_-1.jpg',
+    alt: 'foto',
+    title: 'Dicas para melhorar a presença digital de uma empresa',
+    paragrafh:
+      'Aqui estão algumas dicas: Definir objetivos claros: Antes de começar a investir em marketing digital, é importante definir objetivos claros e mensuráveis para sua presença',
+  },
+  {
+    id: 2,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/02/como-medir-o-sucesso_-1.jpg',
+    alt: 'foto',
+    title: 'Como medir o sucesso de uma campanha de marketing digital?',
+    paragrafh:
+      'Medir o sucesso de uma campanha de marketing digital é importante para avaliar o retorno do investimento e ajustar a estratégia de marketing, se necessário.',
+  },
+  {
+    id: 3,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/02/O-impacto-das-midias-sociais_.jpg',
+    alt: 'foto',
+    title: 'O impacto das mídias sociais na estratégia de marketing de uma empresa.',
+    paragrafh:
+      'As mídias sociais têm um impacto significativo na estratégia de marketing de uma empresa. Aqui estão algumas maneiras pelas quais as mídias sociais podem afetar',
+  },
+  {
+    id: 4,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/02/Tendencias-do-Mkt.jpg',
+    alt: 'foto',
+    title: 'Tendências do Marketing Digital',
+    paragrafh:
+      'Existem várias tendências atuais no marketing digital, mas aqui estão algumas das principais: Conteúdo personalizado: Com o grande volume de informações disponíveis na internet, as',
+  },
+  {
+    id: 5,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/02/5-Social-Selling.jpg',
+    alt: 'foto',
+    title: 'Social Selling: a estratégia de vendas nas redes sociais',
+    paragrafh:
+      'O Social Selling é uma estratégia de vendas que utiliza as redes sociais para se relacionar com potenciais clientes, gerar leads e fechar negócios. É',
+  },
+  {
+    id: 6,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/02/4-formigas.jpg',
+    alt: 'foto',
+    title: 'O que podemos aprender com as formigas?',
+    paragrafh:
+      'As formigas são insetos fascinantes que vivem em colônias altamente organizadas e trabalham juntas para sobreviver e prosperar. Há muitas lições que podemos aprender com',
+  },
+];
