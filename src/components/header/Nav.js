d6b1f45f71c38e20aeb55c8c2e06b7ec09628291
Andre Lucas <andrelucca99@gmail.com>
2023-03-17 12:33:55 -0300

    refactor: altera tag img para o component Image

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './styles/nav.css';
+import Image from '../utils/Image';
 
 function Nav() {
   return (
     <section className="menu">
       <figure>
-        <img
-          src="https://formiga.digital/wp-content/uploads/2022/11/LOGO-FORMIGA-DIGITAL-1-1536x401.png"
-          alt="logo"
-        />
+        <Image image="https://formiga.digital/wp-content/uploads/2022/11/LOGO-FORMIGA-DIGITAL-1-1536x401.png" alt="foto do logo" />
       </figure>
       <nav>
         <ul>
