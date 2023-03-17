8d13d2cc692c8707319ce51bef32c91af3e55fe7
Andre Lucas <andrelucca99@gmail.com>
2023-03-17 11:12:50 -0300

    refactor: alteração do componente Clients e add mock

---
+++ src/components/utils/mocks/Clients.js
@@ -0,0 +1,122 @@
+export const clients = [
+  {
+    id: 1,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-unitfour-formiga-digital-removebg-preview-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 2,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-genesis-caixasi-formiga-digital-removebg-preview.png',
+    alt: 'foto',
+  },
+  {
+    id: 3,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-dr-igor-lepski-formiga-digital.png',
+    alt: 'foto',
+  },
+  {
+    id: 4,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/download-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 5,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-logo-innopharma-formiga-digital-formiga-digital-removebg-preview.png',
+    alt: 'foto',
+  },
+  {
+    id: 6,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-core-first-tech-formiga-digital-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 7,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-colegio-caminho-do-sol-formiga-digital-e1657207163399.png',
+    alt: 'foto',
+  },
+  {
+    id: 8,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-navio-homero-formiga-digital-e1657207141453.png',
+    alt: 'foto',
+  },
+  {
+    id: 9,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-formalisa-formiga-digital-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 10,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-buffet-america-formiga-digital-1-e1657207179520.png',
+    alt: 'foto',
+  },
+  {
+    id: 11,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-logistique-formiga-digital-2-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 12,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-servis-formiga-digital-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 13,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-golfe-riacho-grande.png',
+    alt: 'foto',
+  },
+  {
+    id: 14,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-naturale-formiga-digital-removebg-preview.png',
+    alt: 'foto',
+  },
+  {
+    id: 15,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-royal-cosmetics-formiga-digital.png',
+    alt: 'foto',
+  },
+  {
+    id: 16,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-unitfour-formiga-digital-removebg-preview-1.png',
+    alt: 'foto',
+  },
+  {
+    id: 17,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-fid-formiga-digita.png',
+    alt: 'foto',
+  },
+  {
+    id: 18,
+    image:
+      'https://formiga.digital/wp-content/uploads/2023/01/logo-horizontal-qualitty-cases.png',
+    alt: 'foto',
+  },
+  {
+    id: 19,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-diex-formiga-digital.png',
+    alt: 'foto',
+  },
+  {
+    id: 20,
+    image:
+      'https://formiga.digital/wp-content/uploads/2022/11/logo-core-first-tech-formiga-digital-1.png',
+    alt: 'foto',
+  },
+];
