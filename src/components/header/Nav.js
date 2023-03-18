0fb8ac152f7fb1a304b399843fd991fa6210b505
Andre Lucas <andrelucca99@gmail.com>
2023-03-18 10:15:40 -0300

    add icons no component Nav

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,12 +1,38 @@
 /* eslint-disable react/jsx-max-depth */
 import React from 'react';
 import './styles/nav.css';
+import {
+  ImFacebook,
+  ImInstagram,
+  ImPhone,
+  ImTwitter,
+  ImWhatsapp,
+  ImYoutube,
+} from 'react-icons/im';
 import Image from '../utils/Image';
 
 function Nav() {
   return (
     <section className="menu">
-      <aside className="menu-redes">rede</aside>
+      <aside className="menu-redes">
+        <section className="menu-redes-info">
+          <p className="nav-tel">
+            <ImPhone />
+            11. 3205-2442
+          </p>
+          <p className="nav-tel">
+            <ImWhatsapp />
+            11. 94128-9701
+          </p>
+          <ImFacebook />
+          <ImInstagram />
+          <ImTwitter />
+          <ImYoutube />
+          <p>EN</p>
+          <p>PT</p>
+          <p>ES</p>
+        </section>
+      </aside>
       <section className="menu-nav">
         <figure>
           <Image image="https://formiga.digital/wp-content/uploads/2022/11/LOGO-FORMIGA-DIGITAL-1-1536x401.png" alt="foto do logo" />
